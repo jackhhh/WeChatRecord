144c69826a59ae2baa822f97ee33edbab715a062
jackhhh <mj@etengpc.com>
2017-11-25 16:38:33 +0800

    Fixed a list may not exist error

--- nervous/api/backend_utils.py
+++ nervous/api/backend_utils.py
@@ -6,7 +6,10 @@ def verify_wx_name(wx_name):
     try:
         params = {'wx_name': wx_name}
         d = getdata.get_dict('/weixin/v1/users', params)
-        return d['data'][0]['wx_name'] == wx_name
+        if d['data'] == []:
+            return False
+        else:
+            return True
     except Exception as e:
         traceback.print_exc()
         return None
